09ea0be36efa435ae1fd6328b17beecc479e649a
ImranHossain5722 <imd572258@gmail.com>
2022-10-04 02:54:47 +0600

    build body

---
+++ src/Components/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HomePageVideos } from '../Types';
+
+const Card = ({data}: {data: HomePageVideos}) => {
+    // const isData =data ? true :false
+
+    return (
+        <div>
+            card
+        </div>
+    );
+};
+
+export default Card;
--- src/store/reducers/getHomePageVideos.ts
+++ src/store/reducers/getHomePageVideos.ts
@@ -7,15 +7,20 @@ import { parseData } from "../../utils";
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
-export const getHomePageVideos=createAsyncThunk("youtubeApp/homePageVidoes",async(isNext:boolean,{getState})=>{
+export const getHomePageVideos=createAsyncThunk("youtubeApp/HomePageVideos",async(isNext:boolean,{getState})=>{
 
     const {
         youtubeApp:{nextPageToken: nextPageTokenFromState, videos},
 
     }= getState() as RootState;
 
-    const {data:{items,nextPageToken}}= await axios.get(`${youtube_api_url}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video`);
-    
+    const {
+        data: { items, nextPageToken },
+      } = await axios.get(
+        `${youtube_api_url}/search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=snippet&type=video&${
+          isNext ? `pageToken=${nextPageTokenFromState}` : ""
+        }`
+      )
     const parsedData:  HomePageVideos[] =await parseData(items);
     return{parsedData:[...videos,  ...parsedData],nextPageToken}
 })
