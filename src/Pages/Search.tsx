e4f15b2137f3913d0734cd9f86eaeb886f52ae28
ImranHossain5722 <imd572258@gmail.com>
2022-10-04 06:20:16 +0600

    search page buils done

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
-
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import SearchCard from "../Components/SearchCard";
+import Navbar from "../Components/Navbar";
+import Sidebar from "../Components/Sidebar";
+import Spinner from "../Components/Spinner";
+import { clearVideos } from "../store";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { HomePageVideos } from "../Types";
+import { useNavigate } from "react-router-dom";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
 const Search = () => {
-    return (
-        <div>
-            <h1>search</h1>
-        </div>
-    );
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state?.youtubeApp?.videos);
+  const searchTerm = useAppSelector((state) => state?.youtubeApp?.searchTerm);
+
+  // data load
+  useEffect(() => {
+    dispatch(clearVideos());
+    if (searchTerm === "") navigate("/");
+    else {
+      dispatch(getSearchPageVideos(false));
+    }
+  }, [dispatch, navigate, searchTerm]);
+
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5hv" }}>
+        <Navbar />
+      </div>
+      <div className="flex " style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <div className="py-8 pl-8 flex flex-col gap-5 w-full">
+            <InfiniteScroll
+              dataLength={videos.length}
+              next={() => dispatch(getSearchPageVideos(true))}
+              hasMore={videos.length < 500}
+              loader={<Spinner />}
+              height={600}
+            >
+              {videos.map((item: HomePageVideos) => {
+                return (
+                  <div className="my-5">
+                    <SearchCard data={item} key={item.videoId} />
+                  </div>
+                );
+              })}
+            </InfiniteScroll>
+          </div>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
